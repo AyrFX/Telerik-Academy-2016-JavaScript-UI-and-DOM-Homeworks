bcb678eb6bd644e7564a9a88cf81b77c981cb95b
AyrFX <oleg@gbg.bg>
2016-07-29 15:15:36 +0300

    jQuery plugins homework, taks 2: code for creating of the button added

--- 04. jQuery Plugins/task-2/picker.js
+++ 04. jQuery Plugins/task-2/picker.js
@@ -1,3 +1,16 @@
 $.fn.colorpicker = function () {
     var $this = $(this);
+
+    //construct colorpicker elements
+    var $button = $('<div />')
+        .css({
+            'width': '32px',
+            'height': '32px',
+            'background-image': 'url("imgs/icon.jpg")',
+            'background-size': '32px',
+            'border': '2px solid #626262',
+            'border-radius': '3px'
+        });
+
+    $this.append($button);
 };
