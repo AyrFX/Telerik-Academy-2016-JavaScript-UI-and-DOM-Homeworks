6f70385c085d67c7a6ba3e1dce5b62aa87a75acb
AyrFX <oleg@gbg.bg>
2016-08-03 22:36:24 +0300

    HTML Templates homework: added task 3

--- 05. HTML Templates/tasks/task-3.js
+++ 05. HTML Templates/tasks/task-3.js
@@ -1,9 +1,20 @@
-function solve(){
-  return function(){
-    $.fn.listview = function(data){
-      
+function solve() {
+    return function () {
+        $.fn.listview = function (data) {
+            var $this = $(this),
+                templateId = $this.attr('data-template'),
+                $textTemplate = $('#' + templateId),
+                template = handlebars.compile($textTemplate.html()),
+                result = '',
+                i;
+
+            for (i = 0; i < data.length; i += 1) {
+                result += template(data[i]);
+            }
+
+            $this.html(result);
+        };
     };
-  };
 }
 
 module.exports = solve;
