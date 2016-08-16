605952f9141cf3cb4ff667d158f6d038d0843e9d
AyrFX <oleg@gbg.bg>
2016-08-16 16:05:26 +0300

    another task... partially

--- 06.Exam Preparation/Sample-exam-1/task-2/task-files/scripts.js
+++ 06.Exam Preparation/Sample-exam-1/task-2/task-files/scripts.js
@@ -1,3 +1,33 @@
 $.fn.tabs = function () {
+    var $this = $(this)
+        .css({
+            'border': '1px solid black',
+            'border-radius': '5px'
+        }),
+        $tabItems = $this.children('.tab-item'),
+        $tabTitles = $this.children('.tab-item-title'),
+        i;
 
+    for (i = 0; i < $tabItems.length; i += 1) {
+        $($tabItems[i])
+            .css({
+                'display': 'inline-block'
+            })
+            .children('.tab-item-content')
+            .css('display', 'none')
+            .children('.tab-item-title');
+    }
+
+    for (i = 0; i < $tabTitles.length; i += 1) {
+        $($tabTitles[i]).css({
+            'display': 'inline-block',
+            'padding': '15px'
+        });
+    }
+
+    /*for (i = 0; i < $tabContents.length; i += 1) {
+        $tabContents[i].css({
+            'display': 'none'
+        });
+    }*/
 };
